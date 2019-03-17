0c4d252ddb46111e4220f9d62f5f834aa9599ee5
kdzhv <kadzhaev.design@gmail.com>
2019-03-17 16:34:18 +0300

    Add files via upload
    Загружает файлы для сайта

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const alan = document.querySelector('.site-header__heading');
+const content = document.querySelector('.content');
+
+window.addEventListener('scroll', function() {
+  alan.style.opacity = '0';
+  alan.style.transform = 'scale(.80)';
+  alan.style.visibility = 'hidden';
+  content.style.opacity = '1';
+})
